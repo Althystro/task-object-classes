28c8037384595413e2b828a1f06ebc83441054a1
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-11-05 14:17:20 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,8 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Course x = new Course("english", "ali", 10);
-//        x.setCourseName("math");
-        System.out.println(x.toString());
+        Course course = new Course("english", "ali", 10);
+        System.out.println(course);
         //        x.courseName = "math";
     }
 
